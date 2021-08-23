9339b3209a9e55e0089ac58c15e4b3ff92022ba9
Nicole Bruneau <71276215+NicoleBP02@users.noreply.github.com>
2021-08-23 12:09:43 -0500

    Test Update 2

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//leer el path de los archivos de entrada y de salida
 int main(int argc, char *argv[]){
 
     for(int i = 0; i < argc; i++){
-        printf("argv[%d]: %s\n",i,argv[i]);
+        printf("argv[%d]: %s\n",i,argv[i]); 
     }
     return EXIT_SUCCESS;
 }
