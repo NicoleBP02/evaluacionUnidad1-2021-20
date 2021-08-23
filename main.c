612284a9fb1c96d0d4550786bdc01f3687d1f111
Nicole Bruneau <71276215+NicoleBP02@users.noreply.github.com>
2021-08-23 12:06:09 -0500

    Test Update

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "student.h"
 
 int main(int argc, char *argv[]){
 
+    for(int i = 0; i < argc; i++){
+        printf("argv[%d]: %s\n",i,argv[i]);
+    }
     return EXIT_SUCCESS;
 }
