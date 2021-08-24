114d2b90170e0fdd65c17d7214904b1eabc8aa86
Nicole Bruneau <nicole_bruneau@hotmail.com>
2021-08-24 17:28:17 -0500

    feature 2 arreglada y uso adecuado de student.c y student.h

--- main.c
+++ main.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
-
-void Feature1(FILE *inFile, FILE *outFile);
-int Feature2(FILE *inFile, FILE *outFile);
-char *create_array(int);
-void destroy_array(char *);
+#include "student.h"
 
 //leer el path de los archivos de entrada y de salida
 int main(int argc, char *argv[]){
@@ -29,66 +25,10 @@ int main(int argc, char *argv[]){
             }
         }
     } 
-    Feature1(inFile, outFile);
-    Feature2(inFile, outFile);
+
+    feature1(inFile, outFile);
+    feature2(inFile, outFile);
+    feature3(inFile, outFile);
 
     return EXIT_SUCCESS;
-}
-void Feature1(FILE *inFile, FILE *outFile){
-    //Feature1: lee la primer línea del archivo de entrada y 
-    //la escribe en la primer línea del archivo de salida.
-    uint8_t data = 0;
-    uint8_t lfcount = 0;
-    while((data = fgetc(inFile)) != EOF){
-        if(data == 10) lfcount++; //el código ASCII para nueva linea es 10
-        if(lfcount < 1){ //copia solo la primera linea en outFile
-            fputc (data, outFile);
-        } 
-        else break; 
-    }
-}
-int Feature2(FILE *inFile, FILE *outFile){
-    //Feature2: lee la segunda línea del archivo de entrada, invierte su orden 
-    //y la escribe en la segunda línea del archivo de salida.
-
-    //reservo espacio en mem para buffer que contendrá data
-    char *buffer = create_array(40); //160 lo que podía pesar una línea llena
-    if(buffer == NULL) return EXIT_FAILURE;
-
-    buffer[0] = 10; //enter... para que comienze en la 2da línea
-    uint8_t data = 0;
-    uint8_t lfcount = 0;
-    uint8_t i = 1;
-    while((data = fgetc(inFile)) != EOF){
-        if(data == 10) lfcount++; 
-        if(lfcount >= 1) break;
-        buffer[i] = data;
-        i++;
-    }
-    //cuento la cantidad de chars para crear otro arreglo 
-    uint8_t cont = 0;
-    for(uint8_t i = 0;i<sizeof(buffer);i++){
-        if(buffer[i] == 0) break;
-        cont++;
-    }
-    //creo un arreglo (que es buffer invertido) y lo escribo en file de salida
-    char *invertido = create_array(cont);
-    invertido[0]= 10;
-    for(uint8_t i = 0;i<cont;i++){
-        if(i != 0){
-            invertido[i] = buffer[cont-i];
-        }
-        fputc (invertido[i], outFile);
-    }
-
-    destroy_array(buffer);
-    destroy_array(invertido);
-    return EXIT_SUCCESS;
-}
-char *create_array(int size){
-    return (char * ) malloc(sizeof(int)* size );
-}
-
-void destroy_array(char *this){
-    free(this);
 }
--- student.c
+++ student.c
@@ -1,3 +1,85 @@
 #include <stdlib.h>
+#include <stdint.h>
 #include "student.h"
 
+void feature1(FILE *inFile, FILE *outFile);
+int feature2(FILE *inFile, FILE *outFile);
+int feature3(FILE *inFile, FILE *outFile);
+char *create_array(int);
+void destroy_array(char *);
+
+void feature1(FILE *inFile, FILE *outFile){
+    //Feature1: lee la primer línea del archivo de entrada y 
+    //la escribe en la primer línea del archivo de salida.
+    uint8_t data = 0;
+    uint8_t lfcount = 0;
+    while((data = fgetc(inFile)) != EOF){
+        if(data == 10) lfcount++; //el código ASCII para nueva linea es 10
+        if(lfcount < 1){ //copia solo la primera linea en outFile
+            fputc (data, outFile);
+        } 
+        else break; 
+    }
+}
+int feature2(FILE *inFile, FILE *outFile){
+    //Feature2: lee la segunda línea del archivo de entrada, invierte su orden 
+    //y la escribe en la segunda línea del archivo de salida.
+
+    //reservo espacio en mem para buffer que contendrá data
+    char *buffer = create_array(300); //lo que podía ocupar en bytes una línea llena 
+    if(buffer == NULL) return EXIT_FAILURE;
+
+    buffer[0] = 10; //enter... para que comienze en la 2da línea
+    uint8_t data = 0;
+    uint8_t lfcount = 0;
+    uint8_t j = 1;
+    while((data = fgetc(inFile)) != EOF){
+        if(data == 10) lfcount++; 
+        if(lfcount >= 1) break;
+        buffer[j] = data;
+        j++;
+        //printf("buffer[%d]: %d\n",j,data);
+    }
+
+    //creo un arreglo (que es buffer invertido) y lo escribo en file de salida
+    char *invertido = create_array(j);
+    invertido[0]= 10;
+    for(uint8_t i = 0;i<j;i++){
+        if(i != 0){
+            invertido[i] = buffer[j-i];
+        }
+        fputc (invertido[i], outFile);
+    }
+
+    destroy_array(buffer);
+    destroy_array(invertido);
+    return EXIT_SUCCESS;
+}
+int feature3(FILE *inFile, FILE *outFile){
+    //Feature3: lee el arreglo de enteros de la tercera línea del archivo de entrada, 
+    //calcula la suma y almacena el resultado en la tercera línea del archivo de salida.
+    uint8_t size = 160;
+    char *nums = create_array(size);
+    uint8_t data = 0;
+    uint8_t lfcount = 0;
+    uint8_t i = 0;
+    while((data = fgetc(inFile)) != EOF){
+        if(data == 10) lfcount++; 
+        if(lfcount >= 1) break; 
+        nums[i] = data;
+        printf("nums[%d]: %d\n",i,nums[i]);
+        i++;
+    }
+    for(uint8_t i = 0; i < size/4;i++){
+        //printf("nums[%d]: %d\n",i,nums[i]);
+    }
+    destroy_array(nums);
+    return EXIT_SUCCESS;
+}
+char *create_array(int size){
+    return (char * ) malloc(sizeof(int)* size );
+}
+
+void destroy_array(char *this){
+    free(this);
+}
