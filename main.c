99b3b9bb51c37ec73edf7e0b66a291542c448cee
Nicole Bruneau <nicole_bruneau@hotmail.com>
2021-08-23 19:40:22 -0500

    lee archivo entrada

--- main.c
+++ main.c
@@ -1,11 +1,43 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
 
 //leer el path de los archivos de entrada y de salida
 int main(int argc, char *argv[]){
 
+    FILE *inFile = NULL; 
+    FILE *outFile = NULL;
     for(int i = 0; i < argc; i++){
-        printf("argv[%d]: %s\n",i,argv[i]);
+        if(i == 1){
+            inFile = fopen(argv[i],"r");
+            if (inFile == NULL){
+            perror("open file fails: ");
+            return(EXIT_FAILURE);
+            }
+        }
+        if(i == 2){
+            outFile = fopen(argv[i], "r+");
+            if (outFile == NULL){
+            perror("out file fails: ");
+            return(EXIT_FAILURE);
+            }
+        }
     }
+
+    char buffer[64];
+    char *status =  NULL;
+
+    do{
+        status = fgets(buffer, sizeof(buffer),inFile);
+        if(status != NULL){
+            printf("%s",buffer);
+        }
+    }while (status !=NULL);
+    printf("\n");
+
+    fclose(inFile);
+
     return EXIT_SUCCESS;
 }
